7048cc72ed59c5022c7300bd7df5550d3faed5b9
Alexey <termin-job@yandex.ru>
2021-02-12 14:45:56 +0300

    05 - 01

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,16 +1,34 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs')
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  const isNoNested = !(pathname.indexOf('/') + 1) // Не очевидно, но лучше в переменной описать что я тут нахожу - являеться ли путь вложеным
+
+  if (!isNoNested) {
+    res.statusCode = 400
+    res.end('No access')
+  }
 
   switch (req.method) {
     case 'GET':
+      if(filepath)
+
+      fs.createReadStream(filepath)
+        .on('error', (err) => {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('No file')
+          }
+        }).on('data', (chunk) => {
+          res.statusCode = 200
+          res.end(chunk)
+        })
 
       break;
 
