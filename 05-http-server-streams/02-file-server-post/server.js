93e3c9d285b9f060fe690a32866b94cd49586bb0
Alexey <termin-job@yandex.ru>
2021-02-17 19:42:36 +0300

    05-02 fix

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -2,20 +2,35 @@ const url = require('url');
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-const { codeBlocks } = require('juice');
-const { Stream } = require('stream');
-const limitSizeStream = require('./LimitSizeStream')
-
+const limitSizeStream = require('./LimitSizeStream');
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+async function createHundler (req, filepath) {
+  const creater = fs.createWriteStream(filepath, { flags:'wx' })
+  const limiter = new limitSizeStream({ limit: 10000, objectMode: true})
+
+  return new Promise((res, rej) => {
+    req.on('aborted', () => res({ code: 404, value: 'DISCONECTION', isError: true }) )
+    .pipe(limiter)
+      .on('error', (err) => {
+        if (err.code === 'LIMIT_EXCEEDED') res({ code: 413, value: 'LIMIT_EXCEEDED', isError: true })
+        res({ code: 500, value: 'LIMIT_UnKnow_error', isError: true })
+      })
+    .pipe(creater)
+      .on('error', (err) => {
+        if (err.code === 'EEXIST') res({ code: 409, value: 'FILE_eexist' })
+        res({ code: 500, value: 'CREATER_UnKnow_error' })
+      })
+      .on('finish', () => res({ code: 201, value: 'Успешно' }) )
+  })
+}
+
+server.on('request', async (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   const isNoNested = !(pathname.indexOf('/') + 1) // Не очевидно, но лучше в переменной описать что я тут нахожу - являеться ли путь вложеным
-
-  const resolveData = JSON.stringify(req.headers)
 
   if (!isNoNested) {
     res.statusCode = 400
@@ -25,25 +40,10 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
-      const streame = fs.createReadStream(filepath)
-      // Файл уже есть
-      streame.on('open', () => {
-        res.statusCode = 409
-        res.end()
-      })
-      // Обработка ошибки - но если файла нет идем дальше
-      streame.on('error', (err) => {
-        if(err.code === 'ENOENT' ) return
-        throw new Error(err)
-      })
-      // Создаем Стрим для записи, чтобы создать файт 
-      // streame.pipe(fs.createWriteStream(filepath))
-      // записываем в файл данные из req
-      // .write(resolveData)
-
-      req.on('aborted', () => {
-        streame.destroy()
-      })
+      const answer = await createHundler(req, filepath)
+      res.statusCode = answer.code
+      res.end(answer.value)
+      if (answer.isError) fs.unlink(filepath, () => {})
       break;
 
     default:
@@ -51,5 +51,4 @@ server.on('request', (req, res) => {
       res.end('Not implemented');
   }
 });
-
 module.exports = server;
