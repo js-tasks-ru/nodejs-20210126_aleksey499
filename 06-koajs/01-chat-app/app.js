c1cba679d7935558b97642861ff26162a3dd77af
Alexey <termin-job@yandex.ru>
2021-02-18 12:17:33 +0300

    [koa.js] 06-01

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
-const EventEmmiter = require('events')
+const EventEmmiter = require('events');
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
-const delayPublish = new EventEmmiter()
+const delayPublish = new EventEmmiter();
 
 router.get('/subscribe', async (ctx, next) => {
     ctx.body = await new Promise(async res => {
